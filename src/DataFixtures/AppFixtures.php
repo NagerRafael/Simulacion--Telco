b35b6192f9cf09e1c6f1748ea20ecf00a16aca26
Rafael Naranjo <naranara@espol.edu.ec>
2023-03-06 16:43:53 -0500

    vendor

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 use App\Entity\Curso;
+use App\Entity\Usuario;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -20,11 +21,19 @@ class AppFixtures extends Fixture
         // $product = new Product();
         // $manager->persist($product);
         $roles=["consumidor","creador"];
+        $estados=["activo", "inactivo", "en construcción"];
         for($i =0; $i<5; $i++){
             $curso = new Curso();
-            $curso -> setEstado($roles(random_int(0,1)));
+            $curso -> setEstado($estados(random_int(0,1)));
             $curso -> setDuración(random_int(10,120));
             #$curso -> setPrecio(10.25.strval($i));
+
+            $usuario = new Usuario();
+            $usuario -> setNombres("Usuario ".strval($i));
+            $usuario -> setApellidos("Apellido ".strval($i));
+            $usuario -> setEmail("Email ".strval($i));
+            $usuario -> setTipo($roles(random_int(0,1)))
+            $manager -> persist($usuario);
             $manager -> persist($curso);
             $manager -> flush();
         };
